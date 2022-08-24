8dc264c82d95f023815c1ab5f45e8d2c0138f303
kodeandre <andre.kodehode@gmail.com>
2022-08-25 01:52:35 +0200

    Major changes, parsing and transformation init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { skills, misc } from './data'
 module.exports = {
-  parser: async (url: string) => {
-    if (typeof url !== "string") {
-      throw new TypeError("Parser requires a URL as a string.");
+  parser: async (username: string[]) => {
+    if (typeof username !== "string") {
+      throw new TypeError("Parser requires one or more usernames as a string or array of strings.");
     }
-    return await fetch(url).then((res) => res.json());
+    fs.readFile("../sample.txt", (err, data) => {console.log(data)});
   }
 }
+const arr = [];
+let player = [];
+fs.createReadStream("./sample.txt").pipe(parse({ delimiter: "\n" })
+  .on("data", (row) => {
+    arr.push(row.toString().split(","));
+  })
+  .on("error", (err) => {
+    console.log(err.message);
+  })
+  .on("end", () => {
+    arr.forEach((item, i) => {
+      player.push({
+        "rank": parseInt(item[0]),
+        ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
+        ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+      })
+    })
+    console.log(player);
+  })
+)
