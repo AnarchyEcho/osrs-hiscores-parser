40358563786049cc6918591ff775a17d05dbd8c8
kodeandre <andre.kodehode@gmail.com>
2022-08-24 22:14:18 +0200

    Changed things

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export async function parser(url: string) {
-  if (typeof url !== "string") throw new TypeError("osrs-hiscores-parser requires a string.")
-  return await fetch(url).then((res) => res.json());
+module.exports = {
+  parser: async (url: string) => {
+    if (typeof url !== "string") {
+      throw new TypeError("osrs-hiscores-parser requires a string.");
+    }
+    return await fetch(url).then((res) => res.json());
+  }
 }
