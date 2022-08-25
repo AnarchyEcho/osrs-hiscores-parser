e105b5e3e4746a1e7906e408990d44121c689cf1
kodeandre <andre.kodehode@gmail.com>
2022-08-26 00:31:04 +0200

    hopeful array attempt

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import { skills, misc } from './data'
 module.exports = {
-  parser: async (username: string[]) => {
-    const users = [username];
+  parser: async (usernames: string[]) => {
     const combinedArr = skills.concat(misc);
-    let usersArr = [];
-
-    if (typeof username !== "string" || Array) {
-      throw new TypeError("Parser requires one or more usernames as a string or array of strings.");
-    }
-
-    users.forEach((user) => {
+    usernames.forEach((user) => {
       const arr = [];
       const player = [];
       fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${user}`)
@@ -27,9 +20,8 @@ module.exports = {
             ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
           })
         })
-        usersArr.push(player)
+        return player;
       })
     })
-    return usersArr;
   }
 }
