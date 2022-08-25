3abba4aacb9830ee48165f95a59bd9e5b88f5c01
kodeandre <andre.kodehode@gmail.com>
2022-08-25 18:18:22 +0200

    Should be a working package with initial functionability

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,29 @@ import { parse } from 'csv-parse';
 import { skills, misc } from './data'
 module.exports = {
   parser: async (username: string[]) => {
+    const arr = [];
+    const player = [];
+    const combinedArr = skills.concat(misc);
+
     if (typeof username !== "string") {
       throw new TypeError("Parser requires one or more usernames as a string or array of strings.");
     }
-    fs.readFile("../sample.txt", (err, data) => {console.log(data)});
-  }
-}
-const arr = [];
-let player = [];
-const combinedArr = skills.concat(misc);
-fs.createReadStream("./sample.txt").pipe(parse({ delimiter: "\n" })
-  .on("data", (row) => {
-    arr.push(row.toString().split(","));
-  })
-  .on("error", (err) => {
-    console.log(err.message);
-  })
-  .on("end", () => {
-    arr.forEach((item, i) => {
-      player.push({
-        "name": combinedArr[i],
-        "rank": parseInt(item[0]),
-        ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
-        ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+
+    fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`)
+    .then((res) => res.text().then((row) => {
+      row.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
+        arr.push(item.split(","));
+      })
+    }))
+    .then(() => {
+      arr.forEach((item, i) => {
+        player.push({
+          "name": combinedArr[i],
+          "rank": parseInt(item[0]),
+          ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
+          ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+        })
       })
     })
-    console.log(player);
-  })
-)
+  }
+}
