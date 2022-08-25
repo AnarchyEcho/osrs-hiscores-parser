becf5db8034b7f157edf94fc489b338a57bc9a51
kodeandre <andre.kodehode@gmail.com>
2022-08-25 23:58:36 +0200

    multi user fetch 1

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,35 @@
 import { skills, misc } from './data'
 module.exports = {
   parser: async (username: string[]) => {
-    const arr = [];
-    const player = [];
+    const users = [username];
     const combinedArr = skills.concat(misc);
+    let usersArr = [];
 
     if (typeof username !== "string") {
       throw new TypeError("Parser requires one or more usernames as a string or array of strings.");
     }
 
-    await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`)
-    .then((res) => res.text().then((row) => {
-      row.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
-        arr.push(item.split(","));
-      })
-    }))
-    .then(() => {
-      arr.forEach((item, i) => {
-        player.push({
-          "name": combinedArr[i],
-          "rank": parseInt(item[0]),
-          ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
-          ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+    users.forEach((user) => {
+      const arr = [];
+      const player = [];
+      fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${user}`)
+      .then((res) => res.text().then((row) => {
+        row.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
+          arr.push(item.split(","));
         })
+      }))
+      .then(() => {
+        arr.forEach((item, i) => {
+          player.push({
+            "name": combinedArr[i],
+            "rank": parseInt(item[0]),
+            ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
+            ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+          })
+        })
+        usersArr.push(player)
       })
     })
-    return player;
+    return usersArr;
   }
 }
