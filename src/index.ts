c8363114b812562126c11c43d2fcd07467ee992f
kodeandre <andre.kodehode@gmail.com>
2022-08-25 18:40:15 +0200

    removed sample file

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { skills, misc } from './data'
 module.exports = {
   parser: async (username: string[]) => {
     const arr = [];
+    const player = [];
     const combinedArr = skills.concat(misc);
 
     if (typeof username !== "string") {
       throw new TypeError("Parser requires one or more usernames as a string or array of strings.");
     }
 
-    fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`)
+    await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`)
     .then((res) => res.text().then((row) => {
       row.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
         arr.push(item.split(","));
       })
     }))
     .then(() => {
-      const player = [];
       arr.forEach((item, i) => {
         player.push({
           "name": combinedArr[i],
