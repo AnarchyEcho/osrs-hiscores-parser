dd0e3cb0f8ee44a6c6868c98d6331687e18758cb
kodeandre <andre.kodehode@gmail.com>
2022-08-26 18:39:03 +0200

    Working array functionality

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,33 @@ import { skills, misc } from './data'
 process.removeAllListeners('warning')
 module.exports = {
   parser: async (usernames: string[]) => {
-    async function fetcher(user) {
-      const arr = [];
-      const player = [];
-      const combinedArr = skills.concat(misc);
-      const init = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${user}`).then((res) => res.text())
-      init.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
-        arr.push(item.split(","));
-      })
-      arr.forEach((item, i) => {
-        player.push({
-          "name": combinedArr[i],
-          "rank": parseInt(item[0]),
-          ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
-          ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
-        })
-      })
-      return player;
+    const combinedArr = skills.concat(misc);
+    const getRawUserData = async (username: string) => {
+      const res = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`).then(res => res.text())
+      return res
     }
-    return usernames.map(async (user) => {
-      await fetcher(user)
-    })
+    const sortLogic = async (rawData) => {
+      const player = [];
+      const arr = []
+      await rawData.toString().split("\n").filter(emptyItem => emptyItem).forEach((item) => {
+          arr.push(item.split(","));
+        })
+        arr.forEach((item, i) => {
+          player.push({
+            "name": combinedArr[i],
+            "rank": parseInt(item[0]),
+            ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
+            ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+          })
+        })
+      return player
+    }
+    const result = async () => {
+      const json = await Promise.all(usernames.map(async (user) => (
+        await sortLogic(await getRawUserData(user))
+      )))
+      return json;
+    }
+    return result;
   }
 }
