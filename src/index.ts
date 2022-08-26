bdf9731cea292ba7205d4aa8ce0d83b6d2a0ad85
kodeandre <andre.kodehode@gmail.com>
2022-08-26 18:48:27 +0200

    fixed return

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ process.removeAllListeners('warning')
 module.exports = {
   parser: async (usernames: string[]) => {
     const combinedArr = skills.concat(misc);
-    const getRawUserData = async (username: string) => {
+    const getRawUserData = async (username) => {
       const res = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`).then(res => res.text())
       return res
     }
     const sortLogic = async (rawData) => {
       const player = [];
       const arr = []
-      await rawData.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
+      await rawData.toString().split("\n").filter(emptyItem => emptyItem).forEach((item) => {
           arr.push(item.split(","));
         })
         arr.forEach((item, i) => {
@@ -24,11 +24,11 @@ module.exports = {
       return player
     }
     const result = async () => {
-      const json = await Promise.all(usernames.map(async (user) => (
+      const res = await Promise.all(usernames.map(async (user) => (
         await sortLogic(await getRawUserData(user))
       )))
-      return json;
+      return res;
     }
-    return result;
+    return result();
   }
 }
