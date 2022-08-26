0e3413923737fb11d7ee6e706b518ba334d68956
kodeandre <andre.kodehode@gmail.com>
2022-08-26 17:30:35 +0200

    here we go again

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,15 @@ import { skills, misc } from './data'
 process.removeAllListeners('warning')
 module.exports = {
   parser: async (usernames: string[]) => {
-    const combinedArr = skills.concat(misc);
-    usernames.forEach((username) => {
+    async function fetcher(user) {
       const arr = [];
       const player = [];
-      fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`)
-      .then((res) => res.text().then((row) => {
-        row.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
-          arr.push(item.split(","));
-        })
-        arr.forEach((item, i) => {
-          player.push({
-            "name": combinedArr[i],
-            "rank": parseInt(item[0]),
-            ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
-            ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
-          })
-        })
-      }))
-      return player;
-    })
-  }
-}
-
-const parser = async (usernames: string[]) => {
-  const combinedArr = skills.concat(misc);
-  let json = usernames.map((username) => {
-    const arr = [];
-    let player = [];
-    fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`)
-    .then((res) => res.text().then((row) => {
-      row.split("\n").filter(emptyItem => emptyItem).map((item) => {
+      const combinedArr = skills.concat(misc);
+      const init = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${user}`).then((res) => res.text())
+      init.split("\n").filter(emptyItem => emptyItem).forEach((item) => {
         arr.push(item.split(","));
       })
-      arr.map((item, i) => {
+      arr.forEach((item, i) => {
         player.push({
           "name": combinedArr[i],
           "rank": parseInt(item[0]),
@@ -43,10 +18,10 @@ const parser = async (usernames: string[]) => {
           ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
         })
       })
-    }))
-    return player;
-  })
-  console.log(json)
-  return json;
+      return player;
+    }
+    return usernames.map(async (user) => {
+      await fetcher(user)
+    })
+  }
 }
-parser(["echogim", "emerald12"])
