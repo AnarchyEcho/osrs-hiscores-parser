8d2aaa495cfe542518a2cc51e47a962165e1e74b
kodeandre <andre.kodehode@gmail.com>
2022-11-13 12:02:19 +0100

    removed await/async in places and final return, bracketed the final return

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,29 @@ process.removeAllListeners('warning')
 * @returns An array containing the users arrays
 */
 export async function parser (usernames: string[]) {
-    const combinedArr = skills.concat(misc);
-    const getRawUserData = async (username: string) => {
-      const res = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`, { mode: 'no-cors', headers: { 'Access-Control-Allow-Origin':'*' } }).then(res => res)
-      return res.text();
-    }
-    const sortLogic = async (rawData) => {
-      const player = [];
-      const arr = []
-      await rawData.toString().split("\n").filter(emptyItem => emptyItem).forEach((item) => {
-          arr.push(item.split(","));
-        })
-        arr.forEach((item, i) => {
-          player.push({
-            "name": combinedArr[i],
-            "rank": parseInt(item[0]),
-            ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
-            ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
-          })
-        })
-      return player
-    }
-    return await Promise.all(usernames.map(async (user) => (
-      await sortLogic(await getRawUserData(user))
-    )))
+  const combinedArr = skills.concat(misc);
+  const getRawUserData = async (username: string) => {
+    const res = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`, { mode: 'no-cors', headers: { 'Access-Control-Allow-Origin':'*' } }).then(res => res)
+    return res.text();
   }
+  const sortLogic = (rawData) => {
+    const player = [];
+    const arr = []
+    rawData.toString().split("\n").filter(emptyItem => emptyItem).forEach((item) => {
+        arr.push(item.split(","));
+      })
+      arr.forEach((item, i) => {
+        player.push({
+          "name": combinedArr[i],
+          "rank": parseInt(item[0]),
+          ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
+          ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
+        })
+      })
+    return player
+  }
+  return Promise.all([usernames.map(async (user) => (
+      sortLogic(await getRawUserData(user))
+    ))
+  ])
+}
