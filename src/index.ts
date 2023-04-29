12300b0bb48cd85b842fa5a8a18cbc4d42238d40
kodeandre <andre.kodehode@gmail.com>
2023-04-29 21:29:00 +0200

    updated script

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,30 @@ process.removeAllListeners('warning')
 * @param usernames - rsn of the user or users
 * @returns An array containing the users arrays
 */
-export async function parser (usernames: string[]) {
+export async function parser(usernames: string[]) {
   const combinedArr = skills.concat(misc);
   const getRawUserData = async (username: string) => {
-    const res = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`, { mode: 'no-cors', headers: { 'Access-Control-Allow-Origin':'*' } }).then(res => res)
+    const res = await fetch(`https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`, { mode: 'no-cors', headers: { 'Access-Control-Allow-Origin': '*' } }).then(res => res)
     return res.text();
   }
   const sortLogic = (rawData: string) => {
     const player = [];
     const arr = []
     rawData.toString().split("\n").filter(emptyItem => emptyItem).forEach((item) => {
-        arr.push(item.split(","));
+      arr.push(item.split(","));
+    })
+    arr.forEach((item, i) => {
+      player.push({
+        "name": combinedArr[i],
+        "rank": parseInt(item[0]),
+        ...(player.length < 24 ? { "level": parseInt(item[1]) } : { "score": parseInt(item[1]) }),
+        ...(item[2] !== undefined ? { "experience": parseInt(item[2]) } : {})
       })
-      arr.forEach((item, i) => {
-        player.push({
-          "name": combinedArr[i],
-          "rank": parseInt(item[0]),
-          ...(player.length < 24 ? {"level": parseInt(item[1])} : {"score": parseInt(item[1])}),
-          ...(item[2] !== undefined ? {"experience": parseInt(item[2])} : {})
-        })
-      })
+    })
     return player
   }
-  return Promise.all(usernames.map(async (user) => (
+  const res = Promise.all(usernames.map(async (user) => (
     sortLogic(await getRawUserData(user))
   )))
+  return await res;
 }
